9b5534b1aa321f1771b76920ec94a25a427826d0
ValluruGowtham216 <117166538+ValluruGowtham216@users.noreply.github.com>
2024-06-02 12:01:35 +0530

    Update settings.py

--- FTPintegrator/settings.py
+++ FTPintegrator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ptgh+6gutxspy$*c+fxv6owgalk)rd)*#fjdpqkxvr7g#qa8&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
